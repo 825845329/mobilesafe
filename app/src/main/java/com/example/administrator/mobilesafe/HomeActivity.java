ca23d653c2d1401a33d5f76f3be143ba597156f7
825845329 <825845329@qq.com>
2018-06-14 08:39:18 +0800

    项目提交

---
+++ app/src/main/java/com/example/administrator/mobilesafe/HomeActivity.java
@@ -0,0 +1,96 @@
+package com.example.administrator.mobilesafe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+public class HomeActivity extends Activity{
+
+
+    private GridView gv_content;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        initView();
+
+        initAdapter();
+    }
+
+
+
+    private void initView() {
+       gv_content = findViewById(R.id.gv_content);
+       gv_content.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               switch(position){
+                   case 8:
+                       Intent intent = new Intent(HomeActivity.this,SettingActivity.class);
+                       startActivity(intent);
+                break;
+
+                   default:
+                break;
+               }
+
+
+           }
+       });
+
+    }
+
+    private void initAdapter() {
+        gv_content.setAdapter(new MyAdapter());
+
+    }
+
+
+    private class MyAdapter extends BaseAdapter {
+
+        int[] imageId = { R.drawable.safe, R.drawable.callmsgsafe, R.drawable.app,
+                R.drawable.taskmanager, R.drawable.netmanager, R.drawable.trojan,
+                R.drawable.sysoptimize, R.drawable.atools, R.drawable.settings };
+        String[] names = { "手机防盗", "通讯卫士", "软件管理", "进程管理", "流量统计", "手机杀毒", "缓存清理",
+                "高级工具", "设置中心" };
+        // 设置条目的个数
+
+        @Override
+        public int getCount() {
+            return 9;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = View.inflate(getApplicationContext(), R.layout.activity_home_itme ,null);
+            ImageView  imageView = view.findViewById(R.id.iv_icon);
+            TextView textView = view.findViewById(R.id.tv__text);
+
+            imageView.setImageResource(imageId[position]);
+            textView.setText(names[position]);
+            return view;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+    }
+}
